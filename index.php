8cfa76b6c8332f191f955528c708091e4d48d325
ahmedraye <78651193+ahmedraye@users.noreply.github.com>
2021-02-06 17:21:13 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<div class="container">
+            <div class="btn">all</div>
+            <div class="btn">pineapple</div>
+            <div class="row">
+                <div class="image offset-1 col-3">
+                    <img src="image/164130-84.png">
+                </div>
+                <div class="image offset-1 col-3">
+                    <img src="image/164130-84.png">
+                </div>
+                <div class="image offset-1 col-3">
+                    <img src="image/164130-84.png">
+                </div>
+            </div>
+            <div class="append" rel='test' gugu="huhuhu" data-id="rayes">
+                <p class="tet"> 
+                </p>
+            </div>
+        </div>
